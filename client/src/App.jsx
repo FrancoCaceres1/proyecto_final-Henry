6bdb528ae78f53e5a7934abc49e0573ec4852220
Franco León Cáceres <francocaceres008@gmail.com>
2023-07-17 13:19:08 -0300

    Deploy Railway

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import * as actions from "./redux/actions";
 import axios from "axios";
 import "./App.css";
 
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://proyectofinal-henry-production.up.railway.app/";
 
 function App() {
 
